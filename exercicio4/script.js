e09bb53a26ee3b3f84edb516e8be41a6dfd43236
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-02-01 10:07:00 -0300

    exercicio 04

---
+++ exercicio4/script.js
@@ -0,0 +1,18 @@
+function calcular() {
+    const num = document.querySelector('#num')
+    const res = document.querySelector('#res')
+    const tabuada = document.querySelector('#tabu')
+    if(num.value.length == 0) {
+        window.alert('Coloque um número')
+    } else {
+        tabuada.innerHTML = ''
+        for(let i = 1; i <=10; i++) {
+            
+            const calculo = Number(num.value) * i
+            const opt = document.createElement('option')
+            opt.innerHTML = `${Number(num.value)}x${i} = ${calculo}`
+            
+            tabuada.appendChild(opt)
+        }
+    }
+}
